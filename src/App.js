5733e3edc14cde37042ad22b1d41412e022870a1
Ryan <straightwind0726@gmail.com>
2023-07-10 22:30:42 +0900

    [Update] First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import HeaderTitle from "./components/HeaderTitle";
+import "./css/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HeaderTitle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const GreetingContainer = styled.section`
+  position: relative;
+`;
+
+export default function Greeting() {
+  return <div>Greeting</div>;
+}
